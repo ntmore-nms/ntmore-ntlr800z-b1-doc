e05e79134e29cc757c646901fde8c9ae77f1e0b6
jclee <kmorkah@gmail.com>
2026-09-09 14:26:20 +0900

    update sources

--- source/conf.py
+++ source/conf.py
@@ -39,7 +39,7 @@ html_favicon = 'images/favicon.png'
 
 #html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 html_theme_options = {
     'prev_next_buttons_location': 'bottom',
